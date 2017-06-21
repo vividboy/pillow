b0ac6c07f8b22621bba8611e70da46c9cfd2015a
xudafeng <xudafeng@126.com>
2017-06-21 10:46:45 +0800

    add audio

---
+++ examples/audio/index.js
@@ -0,0 +1,37 @@
+/* global pillow */
+
+;(function(global, P) {
+  var Screen = P.Screen;
+  var Timer = P.Timer;
+  var Text = P.Text;
+  var FPSBoard = P.FPSBoard;
+
+  var fpsBoard = new FPSBoard({
+    container: 'body',
+    width: 100,
+    height: 60,
+    boardColor: '#222',
+    textColor: '#d2ff1d',
+    containerStyles: {
+      position: 'absolute'
+    }
+  });
+
+  var screen = new Screen({
+    container: document.querySelector('#screen'),
+    width: 200,
+    height: 100,
+    x: 0,
+    y: 0
+  });
+
+  var timer = new Timer({
+    fps: 60
+  });
+  timer.update(function() {
+    fpsBoard.tick();
+    screen.run();
+  });
+  timer.start();
+
+})(window, pillow);
